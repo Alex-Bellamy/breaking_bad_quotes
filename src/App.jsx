cb6e24cdf18183bd550f6fb7d63b6ae00d19852a
Alex_Bellamy <alexlbellamy@gmail.com>
2020-07-30 14:51:31 +0200

    adding api fetch request

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    fetch("https://www.breakingbadapi.com/api/quotes")
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          Loaded: true,
+          quotes: json,
+        });
+      });
+  }
+  render() {
+    return <div></div>;
+  }
 }
 
-export default App;
+export default App
